f0bb571591fe3b20cc8170e50bfe390cdc761ecc
Blonded <Lena@Lenas-MacBook-Pro.local>
2018-06-07 15:25:39 -0700

    added ppl to friends list with photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,6 +14,86 @@ var friendList = [
       "3",
       "4"
     ]
+  },
+  {
+    name: "JOLINE",
+    photoUrl: "https://is1-ssl.mzstatic.com/image/thumb/Purple/v4/d5/79/73/d579733e-384f-3e8e-0abd-71e4d914684e/mzl.igohgqyx.png/246x0w.jpg",
+    scores: [
+      "4",
+      "5",
+      "4",
+      "2",
+      "2",
+      "1",
+      "1",
+      "4",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "JESSICA",
+    photoUrl: "https://lh3.googleusercontent.com/-xm1MszSAhcc/Tq4yxgvt6oI/AAAAAAALcbM/TqMunffAuLk/w530-h519-n/sweet-jesus-l.png",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "6",
+      "4",
+      "7",
+      "5",
+      "3",
+      "2",
+      "4"
+    ]
+  },
+  {
+    name: "ROSE",
+    photoUrl: "https://vignette.wikia.nocookie.net/memeinfo/images/e/eb/Meme2.png/revision/latest?cb=20110815032708&path-prefix=es",
+    scores: [
+      "1",
+      "2",
+      "2",
+      "5",
+      "5",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  },
+  {
+    name: "JESSIE",
+    photoUrl: "http://www.metalinjection.net/wp-content/uploads/2013/03/me-gusta-mother-of-gusta-l.png",
+    scores: [
+      "1",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "4",
+      "2",
+      "1",
+      "5"
+    ]
+  },
+  {
+    name: "JOSIE",
+    photoUrl: "http://ragemaker.net/images/Neutral/like%20a%20sir.png",
+    scores: [
+      "2",
+      "5",
+      "5",
+      "5",
+      "2",
+      "3",
+      "3",
+      "4",
+      "3",
+      "4"
+    ]
   }
 ];
 
